2847258d46af0d89d5df416a61193d8deafb5957
Matthew WANG <41960338+Matthewow@users.noreply.github.com>
2021-01-24 00:41:02 +0800

    Update football-matches.c

--- 2021/football-matches.c
+++ 2021/football-matches.c
@@ -1,44 +1,49 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#define TEAM_NUM 10
+
 int main()
 {
-    int teams[10][4] = {0};
+    int teams[TEAM_NUM][5] = {0}, count = 0;
     // score of match, Goals For,Goals Against ,Goal Difference 
-    int count = 0;
-    for(int i=1; i<=10; i++){
-        for(int j=1;j<=10;j++){
-            if(i >= j){
+
+    for(int i = 0; i < TEAM_NUM; i++){
+        for(int j = 0; j < TEAM_NUM; j++){
+            if(i >= j)
                 continue;
-        }
-            int a = rand() % 10 % 5;
-            int b = rand() % 10 % 5;
-            teams[i-1][1] += a;
-            teams[i-1][2] += b;
-            teams[j-1][1] += b;
-            teams[j-1][2] += a;
+
+            int a = rand() % 10 % 5, b = rand() % 10 % 5;
+            teams[i][1] += a;
+            teams[i][2] += b;
+            teams[j][1] += b;
+            teams[j][2] += a;
             
             if (a == b){
-                teams[i-1][0] += 1;
-                teams[j-1][0] += 1;
+                teams[i][0] += 1;
+                teams[j][0] += 1;
             }
             if (a > b){
-                teams[i-1][0] += 3;
-                teams[j-1][0] += 0;
+                teams[i][0] += 3;
+                teams[j][0] += 0;
             }
             if (a < b){
-                teams[i-1][0] += 0;
-                teams[j-1][0] += 3;
+                teams[i][0] += 0;
+                teams[j][0] += 3;
             }
             count = count + 1;
-            printf("\nMatch %d: team %d vs team %d",count, i, j);
-            printf("\nresult is %d : %d", a, b);
-            printf("\n------------------");
+            if(j == 10)
+                printf("\n|  Match %d: team %d vs team %d |", count, i + 1, j + 1);
+            else 
+                printf("\n|  Match %d: team %d vs team %d  |",count, i + 1, j + 1);
+            printf("\n|  Result:       %d  :  %d       |", a, b);
+            printf("\n -------------------------------");
       }
     }
 
     printf("\n ========FINAL RANKING========");
-    for(int i=0; i<10; i++){
+    
+    for(int i = 0; i < TEAM_NUM; i++){
         teams[i][3] = teams[i][1] - teams[i][2];
         printf("\nTeam %d's score is %d, Goals Difference is %d", i+1, teams[i][0], teams[i][3]);
     }
