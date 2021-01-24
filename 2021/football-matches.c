6228e5ca6b21205d554001d8ff2df1821134f8fa
Matthew WANG <41960338+Matthewow@users.noreply.github.com>
2021-01-24 20:55:24 +0800

    Update football-matches.c

--- 2021/football-matches.c
+++ 2021/football-matches.c
@@ -6,14 +6,14 @@
 int main()
 {
     int teams[TEAM_NUM][5] = {0}, count = 0;
-    // score of match, Goals For,Goals Against ,Goal Difference 
+    // score of match, Goals For, Goals Against ,Goal Difference 
 
     for(int i = 0; i < TEAM_NUM; i++){
         for(int j = 0; j < TEAM_NUM; j++){
             if(i >= j)
                 continue;
 
-            int a = rand() % 10 % 5, b = rand() % 10 % 5;
+            int a = rand() % 5, b = rand() % 5; // a is Goals for, b is Goals Against
             teams[i][1] += a;
             teams[i][2] += b;
             teams[j][1] += b;
@@ -41,29 +41,31 @@ int main()
       }
     }
 
-    printf("\n ========FINAL RANKING========");
+    printf("\n ======== FINAL RANKING ========");
     
-    for(int i = 0; i < TEAM_NUM; i++)
+    for(int i = 0; i < TEAM_NUM; i++){
         teams[i][3] = teams[i][1] - teams[i][2];
-    
-    for (int i = 0; i < TEAM_NUM; i++){
-        int temp = -1;
-        for(int j = 0; j < TEAM_NUM; j++){
-            if(teams[j][4] == 1)
-                continue;
-            if(temp == -1)
-                temp = j;
-            if(teams[j][0] < teams[temp][0])
-                continue;
-            else if(teams[j][0] == teams[temp][0])
-                continue;
-            else if(teams[j][1] <= teams[temp][1])
-                continue;
-            else
-                temp = j;
-        }
-        teams[temp][4] = 1;
-        printf("\nNo.%d: Team %d, score: %d, Goals Difference: %d, Goals For %d", i+1, temp+1, teams[temp][0], teams[temp][3], teams[temp][1]);
+        printf("\nTeam %d, score: %d, Goals Difference: %d, Goals For %d", i+1, teams[i][0], teams[i][3], teams[i][1]);
     }
+    
+    // for (int i = 0; i < TEAM_NUM; i++){
+    //     int temp = -1;
+    //     for(int j = 0; j < TEAM_NUM; j++){
+    //         if(teams[j][4] == 1)
+    //             continue;
+    //         if(temp == -1)
+    //             temp = j;
+    //         if(teams[j][0] < teams[temp][0])
+    //             continue;
+    //         else if(teams[j][0] == teams[temp][0])
+    //             continue;
+    //         else if(teams[j][1] <= teams[temp][1])
+    //             continue;
+    //         else
+    //             temp = j;
+    //     }
+    //     teams[temp][4] = 1;
+    //     printf("\nNo.%d: Team %d, score: %d, Goals Difference: %d, Goals For %d", i+1, temp+1, teams[temp][0], teams[temp][3], teams[temp][1]);
+    // }
     return 0;
 }
