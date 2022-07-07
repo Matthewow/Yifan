99c09bce3261990efd0527c8169d771a82705686
Yifan0620 <108609163+Yifan0620@users.noreply.github.com>
2022-07-07 21:15:45 +0800

    problems added
    these problems use ordinary solution, two pointers will be learned later.

---
+++ problems/27.移除元素.py
@@ -0,0 +1,18 @@
+#
+# @lc app=leetcode.cn id=27 lang=python3
+#
+# [27] 移除元素
+#
+
+# @lc code=start
+class Solution:
+    def removeElement(self, nums: List[int], val: int) -> int:
+        i,n = 0,len(nums)
+        for j in range(n):
+            if nums[j] != val:
+                nums[i] = nums[j]
+                i += 1
+        return i
+
+# @lc code=end
+
--- problems/977.有序数组的平方.py
+++ problems/977.有序数组的平方.py
@@ -7,5 +7,6 @@
 # @lc code=start
 class Solution:
     def sortedSquares(self, nums: List[int]) -> List[int]:
+        return sorted(num * num for num in nums)
 # @lc code=end
 
